d1ce01da849a880978e57798a555a5cd349f3bb4
Weeke <baileydunlap9@gmail.com>
2023-02-12 09:28:28 -0800

    Update Vulnornot.cs

--- Vulnornot.cs
+++ Vulnornot.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Linq;
+using System.Data.SqlClient;
 using System.Xml.Linq;
 
 namespace VulnOrNot
@@ -11,12 +12,40 @@ namespace VulnOrNot
         {
             Console.WriteLine("Enter a string to search for vulnerabilities:");
             string inputString = Console.ReadLine();
-            string cve = GetCVE(inputString);
+            string cve = QueryDB(inputString);
             Console.WriteLine(cve);
         }
 
-        static string GetCVE(string inputString)
+        static string QueryDB(string inputString)
         {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\temp\\vuln_db.mdf;Integrated Security=True;Connect Timeout=30";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT link FROM vulnerabilities WHERE description LIKE '%{inputString}%'";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            Console.WriteLine("Vulnerability found! The link is:");
+                            reader.Read();
+                            return reader.GetString(0);
+                        }
+                        else
+                        {
+                            return "No vulnerabilities found for the given string.";
+                        }
+                    }
+                }
+            }
+        }
+
+        static void DownloadXML()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
             // Scrape the ghdb.xml file from the exploit database
             WebClient client = new WebClient();
             string xmlData = client.DownloadString("https://gitlab.com/exploit-database/exploitdb/-/blob/main/ghdb.xml");
@@ -24,24 +53,50 @@ namespace VulnOrNot
             // Load the xml data into a XDocument
             XDocument xmlDoc = XDocument.Parse(xmlData);
 
-            // Find any elements in the xml file that contain the input string
-            var elements = xmlDoc.Descendants("entry")
-                                 .Where(x => x.Value.Contains(inputString));
-
-            if (elements.Count() > 0)
+            // Update the SQL database with the contents of the xml file
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\temp\\vuln_db.mdf;Integrated Security=True;Connect Timeout=30";
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Vulnerability found! The link is:");
-                foreach (var element in elements)
+                connection.Open();
+                foreach (var element in xmlDoc.Descendants("entry"))
                 {
-                    return element.Element("link").Value;
+                    string description = element.Element("textualDescription").Value;
+                    string link = element.Element("link").Value;
+                    string query = $"INSERT INTO vulnerabilities (description, link) VALUES ('{description}', '{link}')";
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
                 }
             }
-            else
-            {
-                return "No vulnerabilities found for the given string.";
-            }
+        }
 
-            return "";
+        static void CreateDB()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string createDBQuery = "IF DB_ID('vuln_db') IS NULL BEGIN CREATE DATABASE vuln_db END";
+
+                using (SqlCommand cmd = new SqlCommand(createDBQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                string useDBQuery = "USE vuln_db";
+                using (SqlCommand cmd = new SqlCommand(useDBQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                string createTableQuery = "IF OBJECT_ID('vulnerabilities', 'U') IS NULL BEGIN CREATE TABLE vulnerabilities (description NVARCHAR(MAX), link NVARCHAR(MAX)) END";
+                using (SqlCommand cmd = new SqlCommand(createTableQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
